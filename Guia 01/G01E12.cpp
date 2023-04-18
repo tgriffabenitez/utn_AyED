f9795fbbd0593ae06afe470029da9db479663682
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-18 20:44:39 -0300

    Arregle el numero de ejercicio

--- Guia 01/G01E12.cpp
+++ Guia 01/G01E12.cpp
@@ -1,5 +1,5 @@
 /*
-    Dada una lista de n, valores enteros, encontrar el mayor valor y su posici�n.
+	Dada una lista de n, valores enteros, encontrar el mayor valor y su posición.
 */
 
 #include<fstream>
@@ -7,27 +7,19 @@ using namespace std;
 
 int main(void)
 {
-    int lista[] = {1,2,3,4,5,6,7,8,9,10};
-    int indice = -1, longitud, n;
+	int lista[] = {1,2,9,3,1,2,56,6,4,21,34};
+	int maximo = lista[0], indice = 0;
+	int	lista_longitud = sizeof(lista)/sizeof(lista[0]);
 
-    longitud = sizeof(lista)/sizeof(lista[0]);
+	for(int i = 1; i < lista_longitud; i++) {
+		if(maximo < lista[i]) {
+			maximo = lista[i];
+			indice = i;
+		}
+	}
+	
+	cout << "El maximo numero de la lista es: " << maximo << " y esta en la posicion: "	<< indice << endl;
 
-    cout << "Ingrese el numero a buscar en la lista: ";
-    cin >> n;
 
-    for(int i = 0; i < longitud; i++) {
-        if (n == lista[i]) {
-            indice = i;
-            break; // devuelvo la posicion del primer valor encontrado
-        }
-    }
-
-    if (indice == -1) {
-        cout << "El numero " << n << " no se encontro en la lista." << endl;
-
-    } else {
-        cout << "El numero " << n << " se encontro en la posicion: " << (indice + 1) << endl;
-    }
-
-    return 0;
+	return 0;
 }
